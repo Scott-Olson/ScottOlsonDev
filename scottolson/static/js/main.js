662b04dc4af55669df64b79335ddfaf596ef0279
scott-olson <scotteolson3@gmail.com>
2020-06-16 13:15:37 -0700

    adjust media url and add js function for copy to clipboard

--- scottolson/static/js/main.js
+++ scottolson/static/js/main.js
@@ -2,3 +2,11 @@
 //   var $nav = $(".fixed-top");
 //   $('nav').toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 // });
+
+function emailCopyFunction() {
+  var copyText = document.getElementById("emailLink");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
+}
